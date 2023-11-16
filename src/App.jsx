856694cca47d1222fc3646e82f022e7f31099f45
Ryan <ryeean16@gmail.com>
2023-11-15 17:51:12 -0800

    chat box added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const sendMessage = () => {
+    if (message.trim() !== '') {
+      setMessages([...messages, message]);
+      setMessage('');
+    }
+  };
+
   return (
     <div className="App">
+      <div className="chat-window">
+        <div className="chat-messages">
+          {messages.map((msg, index) => (
+            <div key={index} className="message">{msg}</div>
+          ))}
+        </div>
+        <div className="message-input">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+  
